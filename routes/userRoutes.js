6ffb47be8f1838c554d60c0c7b9fae342e9f5243
Ruchira tharupathi <ruchiratharupathi01@gmail.com>
2025-05-14 20:18:05 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,38 +1,62 @@
-import express from "express";
- 
-import {  getUser, login, logout, register, updateProfile,  } from "../controllers/userControllers.js";
-import isAuthenticated from "../middlewares/isAuthenticated.js";
-import { singleUpload } from "../middlewares/multer.js";
-
-
-
-const userRouter =express.Router();
-
-userRouter.route("/register").post(singleUpload, register);
-userRouter.route("/login").post(login);
-userRouter.route("/logout").get(logout);
-userRouter.route("/currentuser").get (isAuthenticated, getUser);
-userRouter.route("/profile/update").post(isAuthenticated,singleUpload,updateProfile);
-
-
-export default  userRouter 
-
-
-
-
 // import express from "express";
-// import { getUser, login, logout, register, updateProfile, } from "../controllers/userControllers.js";
+ 
+// import {  getUser, login, logout, register, updateProfile,  } from "../controllers/userControllers.js";
 // import isAuthenticated from "../middlewares/isAuthenticated.js";
 // import { singleUpload } from "../middlewares/multer.js";
 
-// const userRouter = express.Router();
+
+
+// const userRouter =express.Router();
 
 // userRouter.route("/register").post(singleUpload, register);
 // userRouter.route("/login").post(login);
 // userRouter.route("/logout").get(logout);
 // userRouter.route("/currentuser").get (isAuthenticated, getUser);
 // userRouter.route("/profile/update").post(isAuthenticated,singleUpload,updateProfile);
+
+
+
+// export default  userRouter 
+
+
+
+
+// // import express from "express";
+// // import { getUser, login, logout, register, updateProfile, } from "../controllers/userControllers.js";
+// // import isAuthenticated from "../middlewares/isAuthenticated.js";
+// // import { singleUpload } from "../middlewares/multer.js";
+
+// // const userRouter = express.Router();
+
+// // userRouter.route("/register").post(singleUpload, register);
+// // userRouter.route("/login").post(login);
+// // userRouter.route("/logout").get(logout);
+// // userRouter.route("/currentuser").get (isAuthenticated, getUser);
+// // userRouter.route("/profile/update").post(isAuthenticated,singleUpload,updateProfile);
  
 
 
-// export default userRouter;
+// // export default userRouter;
+
+
+
+import express from "express";
+import { deleteUserByAdmin, getAllUsers, getUser, login, logout, register, updateProfile, } from "../controllers/userControllers.js";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import { singleUpload } from "../middlewares/multer.js";
+import { deleteUser } from "../controllers/userControllers.js";
+const userRouter = express.Router();
+
+userRouter.route("/register").post(singleUpload, register);
+userRouter.route("/login").post(login);
+userRouter.route("/logout").get(logout);
+userRouter.route("/currentuser").get (isAuthenticated, getUser);
+userRouter.route("/profile/update").post(isAuthenticated,singleUpload,updateProfile);
+userRouter.route("/delete").delete(isAuthenticated,deleteUser);
+userRouter.route("/getAllUsers").get(isAuthenticated,getAllUsers)
+userRouter.route("/delete/:userId").delete(isAuthenticated,deleteUserByAdmin);
+
+
+
+export default userRouter;
+ 
