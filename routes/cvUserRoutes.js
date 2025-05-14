b3efd969cc5a24f5eaf3afaee4f39531f14c7c77
Damidu Dissanayake <damidunayanajith2001@gmail.com>
2025-05-14 14:39:12 +0530

    cvUser controller and routes added

--- routes/cvUserRoutes.js
+++ routes/cvUserRoutes.js
@@ -1,11 +1,35 @@
 import express from "express";
-import { createCvUserDetails, deletecvuser, getcvUsers } from "../controllers/cvUserControllers.js";
+import {
+  createCvUserDetails,
+  deletecvuser,
+  getAllDetails,
+  getcvUserById,
+  getcvUsers,
+  updateCertificate,
+  updateCvUserDetails,
+  updateEducation,
+  updateExperience,
+  updateProjects,
+  updateReferees,
+  updateUserSkills,
+  updatUserCvID,
+  upload,
+} from "../controllers/cvUserControllers.js";
 
-const cvUserRoutes = express.Router()
+const cvUserRoutes = express.Router();
 
-cvUserRoutes.post("/",createCvUserDetails);
-cvUserRoutes.get("/:userId",getcvUsers)
-cvUserRoutes.delete("/:userId",deletecvuser)
+cvUserRoutes.post("/", createCvUserDetails);
+cvUserRoutes.get("/:userId", getcvUsers);
+cvUserRoutes.delete("/:userId", deletecvuser);
+cvUserRoutes.get("/UserAllCv/:userId", getAllDetails);
+cvUserRoutes.put("/updateCvID/:userId/:cvId", updatUserCvID);
+cvUserRoutes.put("/createDetails/:cvUserId", updateUserSkills);
+cvUserRoutes.put("/updateCertification/:cvUserId", updateCertificate);
+cvUserRoutes.put("/updateProjects/:cvUserId", updateProjects);
+cvUserRoutes.put("/updateExperience/:cvUserId", updateExperience);
+cvUserRoutes.put("/updateEducation/:cvUserId", updateEducation);
+cvUserRoutes.put("/updateReference/:cvUserId", updateReferees);
+cvUserRoutes.get("/getUsers/:cvUserId", getcvUserById);
+cvUserRoutes.put("/updateUser/:cvUserId", upload, updateCvUserDetails);
 
-
-export default cvUserRoutes
+export default cvUserRoutes;
